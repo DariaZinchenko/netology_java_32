bd053a7f5dab0d3a9fb351d6be602a54e5ca040f
Daria Zinchenko <dariadaria0593@gmail.com>
2020-11-29 22:14:49 +0300

    add variable minAmountInKopecks

--- src/MobileOperatorBonus.java
+++ src/MobileOperatorBonus.java
@@ -1,21 +1,22 @@
 public class MobileOperatorBonus {
     public static void main (String[] args) {
         int currentAmountInKopecks = 100_87;
-        int amountInKopecks = 97_43;
+        int amountToReplenishInKopecks = 97_43;
         int coefficient = 100;
+        int minAmountInKopecks = 100_00;
 
         System.out.println("Текущий баланс = " + (float)currentAmountInKopecks/100 + " руб.");
-        System.out.println("Сумма пополнения счета = " + (float)amountInKopecks/100 + " руб.");
+        System.out.println("Сумма пополнения счета = " + (float)amountToReplenishInKopecks/100 + " руб.");
 
-        int bunusInKopecks = calculateBonus(amountInKopecks, coefficient);
-        currentAmountInKopecks += bunusInKopecks + amountInKopecks;
+        int bunusInKopecks = calculateBonus(amountToReplenishInKopecks, minAmountInKopecks, coefficient);
+        currentAmountInKopecks += bunusInKopecks + amountToReplenishInKopecks;
 
         System.out.println("Начисленные бонусы = " + (float)bunusInKopecks/100 + " руб.");
         System.out.println("Баланс после пополненния = " + (float)currentAmountInKopecks/100 + " руб.");
     }
 
-    public static int calculateBonus (int amountInKopecks, int coefficient) {
-        if (amountInKopecks > (100 * 100)) {
+    public static int calculateBonus (int amountInKopecks, int minAmountInKopecks, int coefficient) {
+        if (amountInKopecks > minAmountInKopecks) {
             return (amountInKopecks/coefficient/100) * 100;
         } else {
             return 0;
