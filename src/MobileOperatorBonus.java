da9e94c40c49eb1304481f03081bd807607b0b73
Daria Zinchenko <dariadaria0593@gmail.com>
2020-11-29 21:52:51 +0300

    initial commit

---
+++ src/MobileOperatorBonus.java
@@ -0,0 +1,24 @@
+public class MobileOperatorBonus {
+    public static void main (String[] args) {
+        int currentAmountInKopecks = 100_87;
+        int amountInKopecks = 2535_43;
+        int coefficient = 100;
+
+        System.out.println("Текущий баланс = " + (float)currentAmountInKopecks/100 + " руб.");
+        System.out.println("Сумма пополнения счета = " + (float)amountInKopecks/100 + " руб.");
+
+        int bunusInKopecks = calculateBonus(amountInKopecks, coefficient);
+        currentAmountInKopecks += bunusInKopecks + amountInKopecks;
+
+        System.out.println("Начисленные бонусы = " + (float)bunusInKopecks/100 + " руб.");
+        System.out.println("Баланс после пополненния = " + (float)currentAmountInKopecks/100 + " руб.");
+    }
+
+    public static int calculateBonus (int amountInKopecks, int coefficient) {
+        if (amountInKopecks > 100 * 100) {
+            return (amountInKopecks/coefficient/100) * 100;
+        } else {
+            return amountInKopecks;
+        }
+    }
+}
